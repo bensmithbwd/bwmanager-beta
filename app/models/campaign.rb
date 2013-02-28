9621f31306752996c6c7a705b3ea1998847c5292
Ben <bensmithbwd@gmail.com>
2013-02-28 21:55:23 +0000

    trying again - getting there!

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,4 +7,6 @@ class Campaign < ActiveRecord::Base
   validates :start, :presence => true
   validates :ctype, :presence => true
   validates :url, :presence => true
+  validates_format_of :url, :without => /\Ahttp/
+  validates_format_of :url, :without => /\Awww/
 end
