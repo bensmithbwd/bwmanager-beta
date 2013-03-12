532710d9a33e9c0e65828b1d27450b9867136298
Ben <bensmithbwd@gmail.com>
2013-03-12 02:40:21 +0000

    tried to fix the order of the campaigns on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController  
   def index
-    @campaigns = Campaign.where("DATE_ADD(start, INTERVAL length DAY) <= ? AND complete = 0", 10.days.from_now).order("start ASC")
+    @campaigns = Campaign.where("DATE_ADD(start, INTERVAL length DAY) <= ? AND complete = 0", 10.days.from_now).order("DATE_ADD(start, INTERVAL length DAY) ASC")
     @link_indexes = Campaign.where("links_index <= ?", 10.days.from_now).order("links_index ASC")
     
     #@sql_debug = "@campaigns = " << @campaigns.to_sql << "\n\n" << "@link_indexes = " << @link_indexes.to_sql
